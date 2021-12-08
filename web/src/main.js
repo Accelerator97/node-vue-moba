bf15ea4c401cf36448775bf84165fd89f8e31924
薛斌 <928691436@qq.com>
2021-12-08 20:18:46 +0800

    tab滑动切换 上拉加载

--- web/src/main.js
+++ web/src/main.js
@@ -4,10 +4,11 @@ import './assets/styles/style.scss'
 import router from './router'
 import './assets/iconfont/iconfont.css'
 import axios from 'axios'
+import {List,Tab,Tabs,PullRefresh} from 'vant'
 const http = axios.create({
     baseURL:'http://localhost:3000/web/api'
     // baseURL:  process.env.VUE_APP_API_URl || "/web/api"
 })
 const app = createApp(App)
 app.config.globalProperties.$http = http;
-app.use(router).mount('#app')
+app.use(router).use(List).use(Tab).use(Tabs).use(PullRefresh).mount('#app')
--- web/src/views/matchCenter.vue
+++ web/src/views/matchCenter.vue
@@ -1,87 +1,80 @@
 <template>
-  <div>
-    <div class="nav pb-3 jc-between pt-3">
-      <div
-        class="nav-item mx-2"
-        :class="{ active: currentNavIndex === index }"
-        v-for="(category, index) in matchCates"
-        :key="index"
-        @click="handleClick(index)"
-      >
-        <div class="nav-link">{{ category.name }}</div>
-      </div>
-    </div>
-    <swiper
-      ref="list"
-      @swiper="onSwiper"
-      @slide-change="slideChange"
-      :options="{ autoHeight: true }"
-    >
-      <swiper-slide v-for="(category, index) in matchCates" :key="index">
-        <div class="race-content">
-          <div class="d-flex mb-2 mt-1">
-            <span class="text-blue fs-xxl" style="font-weight: bold"
-              >王者荣耀{{ category.name }}</span
-            >
-            <span class="logo-text pb-2">官方举办</span>
-          </div>
-          <div>
-            <img :src="category.banner" class="w-100" />
-          </div>
-          <div
-            class="w-100 bg-grey-2 mt-2 fs-lg text-center"
-            style="color:#a2a2a2;height：28.5px;line-height:28.5px"
+  <van-tabs
+    v-model:active="currentNavIndex"
+    sticky
+    animated
+    swipeable
+    @change="tabchange"
+  >
+    <van-tab v-for="cate in matchCates" :key="cate._id" :title="cate.name">
+      <div class="mt-2">
+        <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
+          <van-list
+            v-model="Loading"
+            @load="pullingUp"
+            offset="300"
+            :finished="isFinished"
+            :immediate-check="false"
+            class="list"
           >
-            <span>赛事咨询</span>
-          </div>
-
-          <ul class="pl-0">
-            <li
-              v-for="item in category.information_list"
-              :key="item._id"
-              class="my-1"
-            >
-              <a :href="item.url" style="color: #a2a2a2">
-                <span
-                  class="text-ellipse mr-4 ml-1"
-                  style="width: 85%; vertical-align: middle"
-                  >{{ item.title }}</span
+            <div class="race-content">
+              <div class="d-flex mb-2 mt-1">
+                <span class="text-blue fs-xxl" style="font-weight: bold"
+                  >王者荣耀{{ cate.name }}</span
                 >
-                <span style="width: 15%; vertical-align: middle">{{
-                  beautify(item.updateTime)
+                <span class="logo-text pb-2">官方举办</span>
+              </div>
+              <div>
+                <img :src="cate.banner" class="w-100" />
+              </div>
+              <div
+                class="w-100 bg-grey-2 mt-2 fs-lg text-center"
+                style="color:#a2a2a2;height：28.5px;line-height:28.5px"
+              >
+                <span>赛事咨询</span>
+              </div>
+
+              <ul class="pl-0">
+                <li
+                  v-for="item in cate.information_list"
+                  :key="item._id"
+                  class="my-1"
+                >
+                  <a :href="item.url" style="color: #a2a2a2">
+                    <span
+                      class="text-ellipse mr-4 ml-1"
+                      style="width: 85%; vertical-align: middle"
+                      >{{ item.title }}</span
+                    >
+                    <span style="width: 15%; vertical-align: middle">{{
+                      beautify(item.updateTime)
+                    }}</span>
+                  </a>
+                </li>
+              </ul>
+              <div
+                class="w-100 bg-grey-2 mt-2 fs-lg text-center"
+                style="color:#a2a2a2;height：28.5px;line-height:28.5px"
+              >
+                <span>{{
+                  isFinished
+                    ? "已显示全部内容"
+                    : isLoading
+                    ? "加载中..."
+                    : "上拉加载更多"
                 }}</span>
-              </a>
-            </li>
-          </ul>
-          <div
-            class="w-100 bg-grey-2 mt-2 fs-lg text-center"
-            style="color:#a2a2a2;height：28.5px;line-height:28.5px"
-          >
-            <span>{{
-              isSeeMore
-                ? "已显示全部内容"
-                : isLoding
-                ? "加载中..."
-                : "上拉加载更多"
-            }}</span>
-          </div>
-        </div>
-      </swiper-slide>
-    </swiper>
-  </div>
+              </div>
+            </div>
+          </van-list>
+        </van-pull-refresh>
+      </div>
+    </van-tab>
+  </van-tabs>
 </template>
 
 <script>
-import { List } from "vant";
-import Scroll from "../components/scroll.vue";
 import dayjs from "dayjs";
-import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from "swiper";
-import { Swiper, SwiperSlide } from "swiper/vue";
-import "swiper/swiper.scss";
-import "swiper/components/pagination/pagination.scss";
-SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 export default {
-  components: { Swiper, SwiperSlide },
   data() {
     return {
       matchCates: [],
@@ -96,125 +89,68 @@ export default {
         { img: require("../assets/images/matchBanner/8.jpg") },
       ],
       pageNum: 1,
-      isSeeMore: false,
-      isLoding: false,
+      isFinished: false,
+      isLoading: false,
+      Loading: false,
       currentNavIndex: 0,
-      swiper: null,
+      isTabChange: false,
     };
   },
-  computed: {
-    scroll() {
-      return this.$refs.raceScroll.scroll;
-    },
-  },
   methods: {
+    //获取数据
     async fetchmatchCates() {
       const res = await this.$http.get("/race/information");
       this.matchCates = res.data;
       this.matchCates.map((item, i) => {
         item.banner = this.matchBanners[i].img;
       });
+      console.log(res.data);
     },
     beautify(date) {
       return dayjs(date).format("MM/DD");
     },
+    //下拉更新
     async pullingUp() {
-      this.scroll.refresh();
-      this.isLoding = true;
-      const res = await this.$http.get("/race/information", {
-        params: {
-          id: this.matchCates[this.currentNavIndex]._id,
-          pageNum: this.pageNum,
-        },
-      });
-      // console.log("上拉加载请求回来的数据");
-      // console.log(res.data);
-      this.isLoding = false;
-      // 如果拿到结果则让页数＋1，并重启下拉加载事件
-      // 如果服务器出现错误，则重启下拉加载事件
-      // 如果没有更多数据或页数等于5时，则终止下拉加载
-      if (!res) return this.scroll.finishPullUp();
-      if (res.data.length === 0) return (this.isSeeMore = true);
-      this.matchCates[this.currentNavIndex].information_list.push(...res.data);
-      this.scroll.refresh();
-      this.scroll.finishPullUp();
-      // console.log("请求回来数据合并", this.matchCates);
-      this.pageNum++;
-    },
-    handleClick(index) {
-      this.$nextTick(() => {
-        this.scroll.refresh();
-      });
-      this.swiper.slideTo(index);
-      this.currentNavIndex = index;
-      if (this.matchCates[this.currentNavIndex].information_list.length > 50) {
-        this.pageNum = 5;
-        this.isSeeMore = false;
-      } else if (
-        this.matchCates[this.currentNavIndex].information_list.length <= 50 &&
-        this.matchCates[this.currentNavIndex].information_list.length > 40
-      ) {
-        this.pageNum = 4;
-        this.isSeeMore = false;
-        this.scroll.finishPullUp();
-      } else if (
-        this.matchCates[this.currentNavIndex].information_list.length <= 30 &&
-        this.matchCates[this.currentNavIndex].information_list.length > 20
-      ) {
-        this.pageNum = 3;
-        this.isSeeMore = false;
-        this.scroll.finishPullUp();
-      } else if (
-        this.matchCates[this.currentNavIndex].information_list.length <= 20 &&
-        this.matchCates[this.currentNavIndex].information_list.length > 10
-      ) {
-        this.pageNum = 2;
-        this.isSeeMore = false;
-        this.scroll.finishPullUp();
-      } else {
-        this.pageNum = 1;
-        this.isSeeMore = false;
-        this.scroll.finishPullUp();
+      if (this.pageNum > 5) {
+        this.isLoading = false;
+        this.isFinished = true;
+        return;
       }
-    },
-    slideChange() {
-      this.$nextTick(() => {
-        this.scroll.refresh();
-      });
-      this.scroll.scrollTo(0, 0, 500);
-      this.currentNavIndex = this.swiper.realIndex;
-      if (this.matchCates[this.currentNavIndex].information_list.length > 50) {
-        this.pageNum = 5;
-        this.isSeeMore = false;
-      } else if (
-        this.matchCates[this.currentNavIndex].information_list.length <= 50 &&
-        this.matchCates[this.currentNavIndex].information_list.length > 40
-      ) {
-        this.pageNum = 4;
-        this.isSeeMore = false;
-        this.scroll.finishPullUp();
-      } else if (
-        this.matchCates[this.currentNavIndex].information_list.length <= 30 &&
-        this.matchCates[this.currentNavIndex].information_list.length > 20
-      ) {
-        this.pageNum = 3;
-        this.isSeeMore = false;
-        this.scroll.finishPullUp();
-      } else if (
-        this.matchCates[this.currentNavIndex].information_list.length <= 20 &&
-        this.matchCates[this.currentNavIndex].information_list.length > 10
-      ) {
-        this.pageNum = 2;
-        this.isSeeMore = false;
-        this.scroll.finishPullUp();
-      } else {
-        this.pageNum = 1;
-        this.isSeeMore = false;
-        this.scroll.finishPullUp();
+      if (!this.isTabchange) {
+        this.isLoading = true;
+        const res = await this.$http.get("/race/information", {
+          params: {
+            id: this.matchCates[this.currentNavIndex]._id,
+            pageNum: this.pageNum,
+          },
+        });
+        // console.log("上拉加载请求回来的数据");
+        // console.log(res.data);
+        this.isLoading = false;
+        // 如果拿到结果则让页数＋1，并重启下拉加载事件
+        // 如果服务器出现错误，则重启下拉加载事件
+        // 如果没有更多数据或页数等于5时，则终止下拉加载
+        // if (res.data.length === 0 || this.pageNum > 5) return (this.isFinished = true);
+        this.matchCates[this.currentNavIndex].information_list.push(
+          ...res.data
+        );
+        // console.log("请求回来数据合并", this.matchCates);
+        this.pageNum++;
       }
+      this.isTabchange = false
     },
-    onSwiper(swiper) {
-      this.swiper = swiper;
+    onRefresh() {
+      this.finished = false;
+      this.loading = true;
+      this.pullingUp();
+      this.isLoading = false; // 下拉加载完成，关闭，不然就会有两个显示加载中的转圈圈。list本身就有一个了
+    },
+    tabchange() {
+      this.isTabchange=true
+      this.pageNum = 1;
+      this.isFinished = false;
+      this.isLoading = false;
+      this.Loading = false;
     },
   },
   created() {
@@ -224,27 +160,34 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-// .scroll {
-//   height: calc(100vh - 3.48rem);
-//   overflow: hidden;
-//   touch-action: none;
-  .nav {
-    overflow-x: auto;
-    overflow-y: hidden;
-    &::-webkit-scrollbar {
-      display: none;
-    }
-    .nav-item {
-      white-space: nowrap;
-    }
+/deep/.van-tabs__wrap {
+  touch-action: manipulation;
+}
+// .tab {
+//   height: calc(100vh);
+// }
+// .list {
+//   height: 100%;
+//   width: 100%;
+//   overflow-y: auto;
+// }
+.nav {
+  overflow-x: auto;
+  overflow-y: hidden;
+  &::-webkit-scrollbar {
+    display: none;
   }
-  .logo-text {
-    border: 1px solid #a2a2a2;
-    border-radius: 50px;
-    min-width: 5.4254rem;
-    color: #a2a2a2;
-    padding: 0.5% 2%;
-    margin-left: 4%;
-    width: auto;
+  .nav-item {
+    white-space: nowrap;
   }
+}
+.logo-text {
+  border: 1px solid #a2a2a2;
+  border-radius: 50px;
+  min-width: 5.4254rem;
+  color: #a2a2a2;
+  padding: 0.5% 2%;
+  margin-left: 4%;
+  width: auto;
+}
 </style>
